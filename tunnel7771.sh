654e958aa10409b0f6d1b6f571bff359f46dcc21
TheusRM <70729901+TheusRM@users.noreply.github.com>
2025-08-14 22:11:22 -0300

    Update tunnel7771.sh

--- tunnel7771.sh
+++ tunnel7771.sh
@@ -2,5 +2,5 @@
 while true
 do
 ssh -N -R 144.22.178.231:7771:localhost:7771 root@144.22.178.231 #o comando ssh seguido dos comandos de reptição, logo em seguida a porta pública, e então o ip da máquina local que vai ser desviado a conexão para ela com a porta
-sleep 5
+sleep 5 #tempo de pausa entre cada aquisição para não sobrecarregar o servidor 
 done
